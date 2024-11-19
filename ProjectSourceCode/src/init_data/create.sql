fd28e1c2e29cb0d863f6ce4b4fb91c5fb8815e5f
adge7706 <adge7706@colorado.edu>
2024-11-19 12:18:54 -0700

    Updating database to hold more user information

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,15 +1,25 @@
+CREATE TYPE feet_inches AS (
+  feet TINYINT,
+  inches TINYINT
+);
+
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    full_name VARCHAR(50),
+    height feet_inches,
+    span feet_inches,
+    age TINYINT,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE climbs (
     climb_id SERIAL PRIMARY KEY,
+    added_by INTEGER NOT NULL
     name VARCHAR(100) NOT NULL,
     location VARCHAR(100),
-    grade VARCHAR(10),
+    grade TINYINT,
     rating DECIMAL(3, 2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -19,7 +29,7 @@ CREATE TABLE ascents (
     climb_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     comment TEXT,
-    suggested_grade VARCHAR(10),
+    suggested_grade TINYINT,
     rating DECIMAL(3, 2),
     ascent_date DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
